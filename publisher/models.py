b401afa41df26ab6db5997919e053ee43e46b1af
ivanprytula <johnnyforester@gmail.com>
2019-11-14 00:54:09 +0200

    first commit celery demo

---
+++ publisher/models.py
@@ -0,0 +1,18 @@
+"""
+Models for publisher app.
+"""
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+class Post(models.Model):
+    """ Simple Post model."""
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField('Created Date', default=timezone.now)
+    title = models.CharField('Title', max_length=200)
+    content = models.TextField('Content')
+    slug = models.SlugField('Slug')
+
+    def __str__(self):
+        return '"%s" by %s' % (self.title, self.author)
