26fdf18f7ffe663d27353f6803dab5b0be085b43
ivanprytula <johnnyforester@gmail.com>
2019-12-05 23:20:28 +0200

    Update Django to v3.0

--- quick_publisher/celery.py
+++ quick_publisher/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quick_publisher.settings')
@@ -15,3 +16,10 @@ celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'send-report-every-single-minute': {
+        'task': 'publisher.tasks.send_view_count_report',
+        'schedule': crontab(),
+    }
+}
